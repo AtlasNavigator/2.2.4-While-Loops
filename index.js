8157d4baf875c35501603e5a9d11b27033fad3a3
sgtpeppers <92605951+AtlasNavigator@users.noreply.github.com>
2021-11-09 21:24:53 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const readLine = require('readline-sync');
+
+// let x = 1;
+// while (x < 10) {
+//   console.log(x);
+//   x++; //Count until x is no longer less than 10
+// }
+
+// let y = 10;
+// while (y < 10) {
+//   console.log(y);
+//   y++;
+// }
+
+// let z = 5;
+// while (z > 1) {
+//   console.log(z);
+//   z++; //Will count forever since z will always be greater than 1
+// }
+
+// let z = 5;
+// while (z > 1) {
+//   console.log(z);
+//   z--; 
+// }
+
+// let w = 5;
+// while (w > 1) {
+//   console.log(w); //Will always be true since there is no condition
+// }
+
+// let w = 5;
+// while (w > 1) {
+//   console.log(w); //Fixes previous infinite loop
+//   w--;
+// }
+
+let userResponse = readLine.question('Enter y or n: ')
+while (userResponse != "y" && userResponse !='n'){
+  console.log('invalid');
+  userResponse = readLine.question('Enter y or n: ');
+} //Force user into compliance, accepts inputs defined only
